5738b99cf4f12aea9017d627658cee83e31045ff
Marco <5439724524@student.cms.k12.nc.us>
2025-12-30 18:35:31 -0500

    Update elements.js

--- elements.js
+++ elements.js
@@ -3,3 +3,11 @@ let user = "";
 
 const login = document.getElementById("login");
 
+login.addEventListener("input", function(event) {
+  user = event.target.value;
+
+  welcome.textContent = user;
+
+});
+  
+
